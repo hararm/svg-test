6f3d63f01a84a1e5ad3540baa64839a11c123ad9
Armen Harutyunyan <armen@addatech.ca>
2018-09-13 15:19:24 -0400

    drawing layer added

--- src/app/svg-editor/svg-editor.component.ts
+++ src/app/svg-editor/svg-editor.component.ts
@@ -8,15 +8,11 @@ import {
     ViewEncapsulation,
     Output,
     EventEmitter,
-    HostListener, ChangeDetectionStrategy, ChangeDetectorRef
+    HostListener, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit
 } from '@angular/core';
 import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
-export interface SvgCircle {
-    cx: number;
-    cy: number;
-    id: string;
-}
+const xmlns = 'http://www.w3.org/2000/svg';
 
 @Component({
     selector: 'app-svg-editor',
@@ -25,75 +21,64 @@ export interface SvgCircle {
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SvgEditorComponent implements OnInit, OnChanges {
-    @ViewChild('wrapper') wrapper: ElementRef;
-    @Input() svg: string;
-    mode = 'dots';
+export class SvgEditorComponent implements OnInit, OnChanges, AfterViewInit {
+    @ViewChild('mainSvg') mainSvg: ElementRef;
+    @Input() svgTemplateLayerString: string;
+    @Input() svgDrawingLayerString: {svg: string};
     @Input() scale: number;
-    stroke_color = 'black';
     @Output() svgChange = new EventEmitter<string>();
+    svgDrawingLayer: SVGElement;
+    svgSafe: SafeHtml;
+    private default_stroke_color = 'black';
+    private draw_mode = 'dots';
     private path;
-    private delay;
     private stop: boolean;
     private cursor = {'x': 0, 'y': 0};
     private delta = {'x': 0, 'y': 0};
+    private default_strokeWidth = 1;
+    private default_circle_radius = 5;
 
-    private strokeWidth = 1;
-
-    isChecked = true;
-    svgSafe: SafeHtml;
-    activeElement: HTMLElement;
-    details: string;
-
-    array: SvgCircle[] = [];
-
-    constructor(private sanitizer: DomSanitizer,  private ref: ChangeDetectorRef) {
+    constructor(private sanitizer: DomSanitizer, private ref: ChangeDetectorRef) {
     }
 
     ngOnInit() {
     }
 
     ngOnChanges(changes) {
-        const svgChanges = changes['svg'];
+        const svgTemplateChanges = changes['svgTemplateLayerString'];
+        const svgDrawChanges = changes['svgDrawingLayerString'];
         const scaleChanges = changes['scale'];
-        if (svgChanges && svgChanges.currentValue) {
-            this.svgSafe = this.sanitizer.bypassSecurityTrustHtml(this.svg);
+        if (svgTemplateChanges && svgTemplateChanges.currentValue) {
+            this.svgSafe = this.sanitizer.bypassSecurityTrustHtml(this.svgTemplateLayerString);
         } else if (scaleChanges && scaleChanges.currentValue) {
             this.scaleSVG(this.scale);
+        } else if (svgDrawChanges && svgDrawChanges.currentValue) {
+            const new_svg = this.mainSvg.nativeElement.querySelector('#new-svg');
+            new_svg.outerHTML = this.svgDrawingLayerString.svg;
+            this.synchZoom();
         }
         this.ref.markForCheck();
     }
 
     action(event) {
-        if (this.mode === 'dots') {
-            this.activeElement = event.target;
-            this.details = event.target.id;
+        if (this.draw_mode === 'dots') {
             const states = event.target.closest('.states');
-
             if (states) {
                 states.parentElement.removeChild(states);
-                const ind = this.array.findIndex((c: SvgCircle) => {
-                    const cx = states.getElementsByTagName('circle')[0].cx.baseVal.value;
-                    const cy = states.getElementsByTagName('circle')[0].cy.baseVal.value;
-                    return c.cx === cx && c.cy === cy;
-                });
-                this.array.splice(ind, 1);
-            } else if (this.isChecked) {
+            } else {
                 this.addCircles(event);
-                // this.isChecked = false;
             }
-            console.log('Array', this.array);
         }
     }
 
     addCircles(event) {
-        const g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+        const g = document.createElementNS(xmlns, 'g');
         g.classList.add('states');
         if (event.target && event.target.id) {
             g.classList.add(event.target.id);
         }
         console.log(event.target.id);
-        const svg = this.wrapper.nativeElement.querySelector('svg');
+        const svg = this.mainSvg.nativeElement.querySelector('#new-svg');
         const viewBox = svg.getAttribute('viewBox').split(' ');
         const offset = svg.getBoundingClientRect();
         const xRatio = (viewBox[2]) / offset.width;
@@ -101,7 +86,8 @@ export class SvgEditorComponent implements OnInit, OnChanges {
         const x = Math.floor((event.pageX - offset.x) * xRatio + -viewBox[0]);
         const y = Math.floor((event.pageY - offset.y) * yRatio + -viewBox[1]);
 
-        const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+        const circle = document.createElementNS(xmlns, 'circle');
+        circle.setAttribute('fill-opacity', '1');
         if (event.target.id === 'a1') {
             circle.setAttribute('fill', 'green');
         } else if (event.target.id === 'b1') {
@@ -114,19 +100,18 @@ export class SvgEditorComponent implements OnInit, OnChanges {
 
         circle.setAttribute('cx', String(x + 2 * viewBox[0]));
         circle.setAttribute('cy', String(y + 2 * viewBox[1]));
-        circle.setAttribute('r', String(3));
+        circle.setAttribute('r', String(this.default_circle_radius));
 
         g.appendChild(circle);
         svg.appendChild(g);
-        this.array.push({id: event.target.id, cx: circle.cx.baseVal.value, cy: circle.cy.baseVal.value});
-        this.svgChange.emit(this.wrapper.nativeElement.innerHTML);
+        this.svgChange.emit(this.mainSvg.nativeElement.innerHTML);
 
     }
 
     @HostListener('document:mousemove', ['$event'])
     onMouseMove(event) {
-        if (this.mode === 'draw') {
-            const svg = this.wrapper.nativeElement.querySelector('svg');
+        if (this.draw_mode === 'draw') {
+            const svg = this.mainSvg.nativeElement.querySelector('#new-svg');
             const offset = svg.getBoundingClientRect();
             const viewBox = svg.getAttribute('viewBox').split(' ');
             const xRatio = (viewBox[2]) / offset.width;
@@ -141,14 +126,14 @@ export class SvgEditorComponent implements OnInit, OnChanges {
 
     @HostListener('document:mousedown', ['$event'])
     onMouseDown(event) {
-        if (this.mode === 'draw') {
-            const svg = this.wrapper.nativeElement.querySelector('svg');
+        if (this.draw_mode === 'draw') {
+            const svg = this.mainSvg.nativeElement.querySelector('#new-svg');
             this.stop = false;
-            this.path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+            this.path = document.createElementNS(xmlns, 'path');
             this.path.setAttribute('fill-opacity', '0');
             this.path.setAttribute('class', 'drawings');
-            this.path.setAttribute('stroke', this.stroke_color);
-            this.path.setAttribute('stroke-width', this.strokeWidth.toString());
+            this.path.setAttribute('stroke', this.default_stroke_color);
+            this.path.setAttribute('stroke-width', this.default_strokeWidth.toString());
             this.path.setAttribute('d', 'M' + this.cursor.x + ' ' + this.cursor.y);
             svg.appendChild(this.path);
             this.draw();
@@ -157,13 +142,13 @@ export class SvgEditorComponent implements OnInit, OnChanges {
 
     @HostListener('document:mouseup', ['$event'])
     onMouseUp(e) {
-        if (this.mode === 'draw') {
+        if (this.draw_mode === 'draw') {
             this.stop = true;
         }
     }
 
     draw() {
-        if (!this.stop && this.mode === 'draw') {
+        if (!this.stop && this.draw_mode === 'draw') {
             if (this.path) {
                 let d = this.path.getAttribute('d');
                 d += ' L' + this.cursor.x + ' ' + this.cursor.y;
@@ -173,25 +158,28 @@ export class SvgEditorComponent implements OnInit, OnChanges {
     }
 
     onModeChanged(event) {
-        this.mode = event.target.value;
+        this.draw_mode = event.target.value;
     }
+
     onStrokeColorChange(event) {
-        this.stroke_color = event.target.value;
+        this.default_stroke_color = event.target.value;
     }
+
     onStrokeWidthChange(event) {
-        this.strokeWidth = event.target.value;
+        this.default_strokeWidth = event.target.value;
     }
+
     onClearAll() {
-        if (this.mode === 'dots') {
-            const svg = this.wrapper.nativeElement.querySelector('svg');
+        if (this.draw_mode === 'dots') {
+            const svg = this.mainSvg.nativeElement.querySelector('#new-svg');
             const states = svg.getElementsByClassName('states');
             if (states) {
                 while (states.length > 0) {
                     states[0].parentNode.removeChild(states[0]);
                 }
             }
-        } else if (this.mode === 'draw') {
-            const svg = this.wrapper.nativeElement.querySelector('svg');
+        } else if (this.draw_mode === 'draw') {
+            const svg = this.mainSvg.nativeElement.querySelector('#new-svg');
             const drawings = svg.getElementsByClassName('drawings');
             if (drawings) {
                 while (drawings.length > 0) {
@@ -203,8 +191,39 @@ export class SvgEditorComponent implements OnInit, OnChanges {
 
     scaleSVG(zoom: number) {
         // get all SVG objects in the DOM
-        const svg = this.wrapper.nativeElement.querySelector('svg');
+        const svg = this.mainSvg.nativeElement.querySelector('svg');
         // set viewable area based on value above
         svg.setAttribute('transform', 'scale(' + zoom + ')');
+        const new_svg = this.mainSvg.nativeElement.querySelector('#new-svg');
+        // set viewable area based on value above
+        new_svg.setAttribute('transform', 'scale(' + zoom + ')');
+    }
+
+    ngAfterViewInit(): void {
+        this.createDrawingLayer();
+    }
+
+    private synchZoom() {
+        const svg = this.mainSvg.nativeElement.querySelector('svg');
+        const drawSvg = this.mainSvg.nativeElement.querySelector('#new-svg');
+        const transform = svg.getAttribute('transform');
+        drawSvg.setAttribute('transform', transform);
+    }
+
+    private createDrawingLayer() {
+        const svg = this.mainSvg.nativeElement.querySelector('svg');
+        const viewBox = svg.getAttribute('viewBox');
+        const width = svg.getAttribute('width');
+        const height = svg.getAttribute('height');
+        this.svgDrawingLayer = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        this.svgDrawingLayer.setAttribute('x', '0');
+        this.svgDrawingLayer.setAttribute('y', '0');
+        this.svgDrawingLayer.setAttribute('width', width);
+        this.svgDrawingLayer.setAttribute('height', height);
+        this.svgDrawingLayer.setAttribute('viewBox', viewBox);
+        this.svgDrawingLayer.setAttribute('class', 'new-svg');
+        this.svgDrawingLayer.setAttribute('id', 'new-svg');
+        this.svgDrawingLayer.style.setProperty('fill-opacity', '0.0', '');
+        this.mainSvg.nativeElement.appendChild( this.svgDrawingLayer);
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
 };
 
 /*
- * In development mode, for easier debugging, you can ignore zone related error
+ * In development draw_mode, for easier debugging, you can ignore zone related error
  * stack frames such as `zone.run`/`zoneDelegate.invokeTask` by importing the
- * below file. Don't forget to comment it out in production mode
+ * below file. Don't forget to comment it out in production draw_mode
  * because it will have a performance impact when errors are thrown
  */
 // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
